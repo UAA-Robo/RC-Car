a67d1c44fef715426226f7816e1026b3eaa59e3c
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-01-07 20:16:55 -0900

    Prevent text from being selected

--- rc_car.ino
+++ rc_car.ino
@@ -107,6 +107,14 @@ String SendHTML(){
             text-align: center;
             font-weight: bold;
             transition: transform 0.1s;
+            
+            /* Prevent arrows from being selected on mobile */
+            -webkit-touch-callout: none; /* iOS Safari */
+            -webkit-user-select: none; /* Safari */
+            -khtml-user-select: none; /* Konqueror HTML */
+            -moz-user-select: none; /* Old versions of Firefox */
+            -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
 
         }
         .arrow:active {
